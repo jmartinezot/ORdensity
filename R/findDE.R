8a879a5203a6ae0818f402f39c4efd3b500dab23
Jose Maria Martinez Otzeta <josemaria.martinezo@ehu.eus>
2019-01-29 13:44:07 +0100

    Keeping fixing code

--- R/findDE.R
+++ R/findDE.R
@@ -1,23 +1,25 @@
-#' @title 
+#' @title
 #' Class for representing findDE
 #'
 #' @description
 #' Class for representing findDE
-#' 
+#'
 #' @name findDE
 #' @exportClass findDE
-#' 
+#'
 #' @author Itziar Irigoien, Concepcion Arenas, Jose Maria Martinez-Otzeta
-#' 
+#'
 #' @export plotFPvsOR
 #' @export plotclusters
 #' @export clusplotk
 #' @export compute.findDE
+#' @export findDEgenes
+#' @export summary
 
 findDE <- setClass(
 	"findDE",
-	slots = c(positive="matrix", negative="matrix", out="list", OR="numeric", FP="numeric", dFP="numeric", char="data.frame", clustering2="integer", clustering4="integer", verbose="logical", parallel="logical"),
-	prototype = list(positive=matrix(), negative=matrix(), out=list(), OR=numeric(), FP=numeric(), dFP=numeric(), char=data.frame(), clustering2=integer(), clustering4=integer(), verbose=logical(), parallel=logical())
+	slots = c(positive="matrix", negative="matrix", out="list", OR="numeric", FP="numeric", dFP="numeric", char="data.frame", bestK = "numeric", clustering2="integer", clustering4="integer", verbose="logical", parallel="logical", replicable="logical", seed="numeric"),
+	prototype = list(positive=matrix(), negative=matrix(), out=list(), OR=numeric(), FP=numeric(), dFP=numeric(), char=data.frame(), bestK = numeric(), clustering2=integer(), clustering4=integer(), verbose=logical(), parallel=logical(), replicable=logical(), seed=numeric())
 )
 
 # setGeneric("summary")
@@ -41,27 +43,16 @@ setMethod("show",
           }
 )
 
-setGeneric("summaries", function(object, ...) standardGeneric("summaries"))
-
-setMethod("summaries",
-          signature = "findDE",
-          definition = function(object){
-            list("summaryOR"=object@OR,
-                 "summaryFP"=object@FP,
-                 "summarydFP"=object@dFP)
-          }
-)
-
-
-
-
 setGeneric("plotFPvsOR", function(object, ...) standardGeneric("plotFPvsOR"))
 
 setMethod("plotFPvsOR",
   signature = "findDE",
-  definition = function(object){
+  definition = function(object, k = object@bestK){
+    clustering <- pam(dist(scale(object@char)), k)$clustering
+    legend_text <- sprintf("cluster %s",seq(1:k))
     plot(object@FP, object@OR, type="n",main="Potential genes",xlab="FP",ylab="OR")
-    points(object@FP, object@OR, cex=1/(0.5+object@dFP))
+    points(object@FP, object@OR, cex=1/(0.5+object@dFP),  col = clustering)
+    legend("topright", legend=legend_text, pch=16, col=unique(clustering))
     }
   )
 
@@ -103,9 +94,9 @@ setMethod("bootstrap",
 	aux2 <- z[, s2]
 
 
-	qx.b <- t(apply(aux1, 1, quantile, probs=c(0.25, 0.5, 0.75))) 
+	qx.b <- t(apply(aux1, 1, quantile, probs=c(0.25, 0.5, 0.75)))
 	#    mx.b <- apply(aux1, 1, mean)
-	qy.b <- t(apply(aux2, 1, quantile, probs=c(0.25, 0.5, 0.75)))  
+	qy.b <- t(apply(aux2, 1, quantile, probs=c(0.25, 0.5, 0.75)))
 	#   my.b <- apply(aux2, 1, mean)
 	mz <- cbind(qx.b - qy.b) #, mx.b-my.b)
 
@@ -122,37 +113,37 @@ setMethod("bootstrap",
 	D0 <- as.matrix(dist(mv.null[,,b]))
 	vgR0 <- median(D0^2)/2
 	I <- apply(D0, 1,  IindexRobust, vg=vgR0)
-	ORnull[, b] <- 1/I 
+	ORnull[, b] <- 1/I
   }
   )
-          
+
 setGeneric("compute.findDE", function(object, ...) standardGeneric("compute.findDE"))
 
-setMethod("compute.findDE", 
+setMethod("compute.findDE",
 	signature = "findDE",
-	definition =  function(object, B=100, scale=FALSE, alpha=0.05, fold=floor(B/10), weights=c(1/4,1/2,1/4), K = 10, verbose=FALSE, parallel = FALSE) {
-		  a <- system.time ({
+	definition =  function(object, B=100, scale=FALSE, alpha=0.05, fold=floor(B/10), weights=c(1/4,1/2,1/4), K = 10, verbose=FALSE, parallel = FALSE, replicable = TRUE, seed = 0) {
+	    a <- system.time ({
 	    x <- as.matrix(object@positive)
 		  y <- as.matrix(object@negative)
 		  G <- dim(x)[1]
-		  N1 <- dim(x)[2] 
+		  N1 <- dim(x)[2]
 		  N2 <- dim(y)[2]
 		  N <- N1 + N2 })
-		  
+
 		  gloN <<- N
 		  gloN1 <<- N1
-		  
+
 		  if (verbose) {print('a'); print(a)}
-		  
+
 		  b <- system.time ({
 		  qx <- t(apply(x, 1, quantile, probs=c(0.25, 0.5, 0.75)))
 		 # mx <- apply(x, 1, mean)
 		  qy <- t(apply(y, 1, quantile, probs=c(0.25, 0.5, 0.75)))
 		 # my <- apply(y, 1, mean)
 		  mv <- cbind(qx - qy) #, mx-my)
-		  
+
 		  p <- dim(mv)[2]
-		  
+
 		  if(scale){
 		    RIx <- qx[,3]-qx[,1]
 		    RIy <- qy[,3]-qy[,1]
@@ -161,40 +152,45 @@ setMethod("compute.findDE",
 		    {stop('Can\'t scale the data')}
 		    mv <- mv/maxRI
 		  } })
-		  
+
 		  if (verbose) {print('b'); print(b)}
-		  
+
 		  c <- system.time ({
 		  mv <- mv*matrix(rep(weights, G), byrow=TRUE, ncol=p)
 		  Dxy <- dist(mv) #ICGE::dgower(mv, type=list(cuant=1:p))
 		  })
-		  
+
 		  if (verbose) {print('c'); print(c)}
-		  
+
 		  d <- system.time ({
 		  z <- cbind(x, y)
 		  ORnull <- matrix(0, nrow=G, ncol=B)
 		  mv.null <- array(0, dim=c(G, p, B))
-		  
+
 		  if (parallel)
 		  {
-		  require(foreach)
+		    require(foreach)
+		    if (replicable){
+		      require(doRNG)
+		      set.seed(seed)
+		    }
+
 		  # require(bigstatsr)
 	    nproc <- parallel::detectCores()
 		  cl <- parallel::makeCluster(nproc)
 		  doParallel::registerDoParallel(cl)
-      res_par <- foreach(b = 1:B, .combine = 'c') %dopar% {
+      res_par <- foreach(b = 1:B, .combine = 'c', .options.RNG=seed) %dorng% {
   			s1 <- sample(1:N, N1, replace=FALSE)
   			s2 <- (1:N)[-s1]
   			aux1 <- z[, s1]
   			aux2 <- z[, s2]
-  
+
   			qx.b <- t(apply(aux1, 1, quantile, probs=c(0.25, 0.5, 0.75))) # se puede tener antes
   			#    mx.b <- apply(aux1, 1, mean)
   			qy.b <- t(apply(aux2, 1, quantile, probs=c(0.25, 0.5, 0.75))) # se puede tener antes
   			#   my.b <- apply(aux2, 1, mean)
   			mz <- cbind(qx.b - qy.b) #, mx.b-my.b)
-  
+
   			if(scale)
   			{
   			RIx.b <- qx.b[,3]-qx.b[,1] # se puede tener antes
@@ -205,7 +201,7 @@ setMethod("compute.findDE",
   			res <- list()
   			# mv.null[ , ,b] <- mz*matrix(rep(weights, G), byrow=TRUE, ncol=p)
   			res[[1]] <- mz*matrix(rep(weights, G), byrow=TRUE, ncol=p)
-  
+
   			#D0 <- as.matrix(ICGE::dgower(mz, type=list(cuant=1:p)))
   			# D0 <- as.matrix(dist(mv.null[,,b]))
   			D0 <- as.matrix(dist(res[[1]]))
@@ -216,9 +212,9 @@ setMethod("compute.findDE",
   			res
 		  }
       parallel::stopCluster(cl)
-      
+
       glo <<- res_par
-      
+
       for (b in 1:B) {
         mv.null[ , ,b] <- res_par[[b*2-1]]
         ORnull[, b] <- res_par[[b*2]]
@@ -226,19 +222,22 @@ setMethod("compute.findDE",
 		} # end if
 		else
 		{
+		  if (replicable){
+        set.seed(seed)
+		  }
 		  for (b in 1:B)
 		  {
 		    s1 <- sample(1:N, N1, replace=FALSE)
 		    s2 <- (1:N)[-s1]
 		    aux1 <- z[, s1]
 		    aux2 <- z[, s2]
-		    
-		    qx.b <- t(apply(aux1, 1, quantile, probs=c(0.25, 0.5, 0.75))) 
+
+		    qx.b <- t(apply(aux1, 1, quantile, probs=c(0.25, 0.5, 0.75)))
 		    #    mx.b <- apply(aux1, 1, mean)
-		    qy.b <- t(apply(aux2, 1, quantile, probs=c(0.25, 0.5, 0.75)))  
+		    qy.b <- t(apply(aux2, 1, quantile, probs=c(0.25, 0.5, 0.75)))
 		    #   my.b <- apply(aux2, 1, mean)
 		    mz <- cbind(qx.b - qy.b) #, mx.b-my.b)
-		    
+
 		    if(scale)
 		    {
 		      RIx.b <- qx.b[,3]-qx.b[,1]
@@ -246,34 +245,34 @@ setMethod("compute.findDE",
 		      maxRI.b <- apply(cbind(RIx.b, RIy.b), 1, max)
 		      mz <- mz/maxRI.b
 		    }
-		    
+
 		    mv.null[ , ,b] <- mz*matrix(rep(weights, G), byrow=TRUE, ncol=p)
-		    
-		    
+
+
 		    #D0 <- as.matrix(ICGE::dgower(mz, type=list(cuant=1:p)))
 		    D0 <- as.matrix(dist(mv.null[,,b]))
 		    vgR0 <- median(D0^2)/2
 		    I <- apply(D0, 1,  IindexRobust, vg=vgR0)
-		    ORnull[, b] <- 1/I  
+		    ORnull[, b] <- 1/I
 		  }
 		}
 		  })
-		  
+
 		  if (verbose) {print('d'); print(d)}
-		  
+
 		# OR values for original data
 		  e <- system.time ({
 		  Dxy <- as.matrix(Dxy)
 		  vgR <- median(Dxy^2)/2
 		  I <- apply(Dxy, 1,  IindexRobust, vg=vgR)
 		  OR <- 1/I
-		  
-		# Find cut point  
+
+		# Find cut point
 		#  M <- apply(ORnull, 2, sort)
 		#  ORnullord <- apply(M, 1, median)
 		#  corte <- ORnullord[floor((1-alpha)*G)]
 		   corte <- (sort(c(ORnull)))[floor((1-alpha)*G*B)]
-		  
+
 		# Find individuals beyond threshold
 		   suspicious <- OR > corte
 		   ns <- sum(suspicious)
@@ -281,13 +280,13 @@ setMethod("compute.findDE",
 		  selec.null <- which(ORnull > corte, arr.ind=TRUE)
 		  nnull <- dim(selec.null)[1]
 		#
-		  mirar <- sample(1:fold, nnull, replace=TRUE) # 
+		  mirar <- sample(1:fold, nnull, replace=TRUE) #
 		  # print(ns)
 		  # print(fold)
 		  apilar <- array(0, dim=c(ns, 3, fold)) })
-		  
+
 		  if (verbose) {print('e'); print(e)}
-		  
+
 		# Que pasa en la capa 1
 		  f <- system.time ({
 		  for (j in 1:fold)
@@ -312,8 +311,8 @@ setMethod("compute.findDE",
 		      Dmix <- as.matrix(Dmix)
 		      res <- matrix(0, nrow=ns, ncol=3)
 		      colnames(res) <- c( "FPneighbourghood", "densityFP", "radius")
-		  
-		 
+
+
 		      Dred <- Dmix[1:ns, ]
 		      for(i in 1:ns)
 		      {
@@ -321,9 +320,9 @@ setMethod("compute.findDE",
 		      }
 		      apilar[ , , j] <- res
 		  } })
-		  
+
 		  if (verbose) {print(f)}
-		  
+
 		  g <- system.time ({
 		   aux <- t(plyr::aaply(apilar, c(2,1), mean))
 		   auxx <- t(apply(apilar[, 1, ], 1, function(x){c(min(x), mean(x), max(x))}))
@@ -338,11 +337,11 @@ setMethod("compute.findDE",
 		   # print(res[oo,])
 		   # print(ns)
 		   # print(c(ps, p0))
-		   # print(list("summary"=res[oo, ], "ns"=ns, "prop"=c(ps, p0))) 
+		   # print(list("summary"=res[oo, ], "ns"=ns, "prop"=c(ps, p0)))
 		   })
-		   
+
 		   if (verbose) {print('g'); print(g)}
-		  
+
 		   object@out <- list("summary"=res[oo, ], "ns"=ns, "prop"=c(ps, p0, K))
 		   # object
 		}
@@ -370,26 +369,29 @@ setValidity("findDE", function(object) {
 }
 )
 
-setMethod("initialize", "findDE", function(.Object, positive, negative, out, OR, FP, dFP, char, clustering2, clustering4, verbose = FALSE, parallel = FALSE) {
+setMethod("initialize", "findDE", function(.Object, positive, negative, out, OR, FP, dFP, char, bestK, clustering2, clustering4, verbose = FALSE, parallel = FALSE, replicable = TRUE, seed = 0) {
   .Object@positive <- positive
   .Object@negative <- negative
   # validObject(.Object)
   .Object@verbose <- verbose
   .Object@parallel <- parallel
-  .Object@out <- compute.findDE(.Object, verbose = .Object@verbose, parallel = .Object@parallel)
+  .Object@replicable <- replicable
+  .Object@seed <- seed
+  .Object@out <- compute.findDE(.Object, verbose = .Object@verbose, parallel = .Object@parallel, seed = .Object@seed)
   .Object@OR <- .Object@out$summary[, "OR"]
   .Object@FP <- .Object@out$summary[, "FP"]
   .Object@dFP <- .Object@out$summary[, "dFP"]
   .Object@char <- data.frame(.Object@OR, .Object@FP, .Object@dFP)
   require(cluster)
+  .Object@bestK <- findbestK(.Object)
   .Object@clustering2 <- pam(dist(scale(.Object@char)), 2)$clustering
   .Object@clustering4 <- pam(dist(scale(.Object@char)), 4)$clustering
   .Object
 })
 
-setGeneric("findDEgenes", function(object, ...) standardGeneric("findDEgenes"))
+setGeneric("findbestK", function(object, ...) standardGeneric("findbestK"))
 
-setMethod("findDEgenes",
+setMethod("findbestK",
           signature = "findDE",
           definition = function(object){
             s <- rep(NA, 10)
@@ -399,26 +401,35 @@ setMethod("findDEgenes",
               s[k] <- mean(silhouette(aux)[, "sil_width"])
             }
             best_k <- which(s == max(s, na.rm = TRUE))
-            clustering <- pam(dist(scale(object@char)), best_k)$clustering
+            return (best_k)
+          }
+)
+
+setGeneric("findDEgenes", function(object, ...) standardGeneric("findDEgenes"))
+
+setMethod("findDEgenes",
+          signature = "findDE",
+          definition = function(object){
+            clustering <- pam(dist(scale(object@char)), object@bestK)$clustering
             result_prov <- list()
             meanOR <- list()
-            for (k in 1:best_k)
+            for (k in 1:object@bestK)
             {
               result_prov[[k]] <- object@out$summary[clustering==k,]
               meanOR[[k]] <- mean(result_prov[[k]][,'OR'])
             }
             clusters_ordering <- order(as.numeric(meanOR), decreasing = TRUE)
             clusters <- list()
-            for (k in 1:best_k)
+            for (k in 1:object@bestK)
             {
               clusters[[k]] <- result_prov[[clusters_ordering[k]]]
             }
             DFgenes <- list()
-            for (k in 1:best_k)
+            for (k in 1:object@bestK)
             {
               DFgenes[[k]] <- list("cluster_number"=k, "genes"=sort(clusters[[k]][,'id']), "meanOR"=mean(clusters[[k]][,'OR']))
             }
-            cat("The ORdensity method has found that the optimal clustering of the data consists of",best_k,"clusters\n")
+            cat("The ORdensity method has found that the optimal clustering of the data consists of",object@bestK,"clusters\n")
             return(list("DFgenes"=DFgenes,"clusters"=clusters))
           }
 )
